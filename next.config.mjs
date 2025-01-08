89840e4b0dc5c4e04569aeb6020966448ac2ce24
zzzzshawn <zzzzshawn@gmail.com>
2025-01-09 02:30:15 +0530

    ggs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 export default nextConfig;
